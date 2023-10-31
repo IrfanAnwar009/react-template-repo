c90597c623bbfa197e4a7d4e1e2cb322976daaf5
Irfan Anwar <irfananwarsoomro95@gmail.com>
2023-10-31 23:28:42 +0500

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+export const Footer =()=>{
+    return (
+        <>
+            <h1>Footer Section</h1>
+        </>
+    );
+}
+
+export const Footer2 =()=>{
+    return (
+        <>
+            <h1>Footer2 Section</h1>
+        </>
+    );
+}
